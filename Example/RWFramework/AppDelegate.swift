d5ae1a96532025ceaf4c8f2f4ce713ab700485d0
Christopher Reed <mail@seeread.info>
2016-02-22 14:39:27 -0500

    POD: make CocoaPod ready - needs check on public header for audio - license discussion - check on unwrapping CLLocation

---
+++ Example/RWFramework/AppDelegate.swift
@@ -0,0 +1,93 @@
+//
+//  AppDelegate.swift
+//  RWExample
+//
+//  Created by Joe Zobkiw on 1/24/15.
+//  Copyright (c) 2015 Roundware. All rights reserved.
+//
+
+import UIKit
+import RWFramework
+import AVFoundation
+import MapKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        // Override point for customization after application launch.
+        // Configure AVAudioSession for the application
+        let avAudioSession = AVAudioSession.sharedInstance()
+        var error: NSError?
+        
+        // This can be moved to the appropriate place in the application where it makes sense
+        avAudioSession.requestRecordPermission { (granted: Bool) -> Void in
+            print("AppDelegate: record permission granted: \(granted)")
+        }
+        
+        // Play and record for VOIP
+        do {
+            try avAudioSession.setCategory(AVAudioSessionCategoryPlayAndRecord)
+        } catch let error1 as NSError {
+            error = error1
+            print("AppDelegate: could not set session category")
+            if let e = error {
+                print(e.localizedDescription)
+            }
+        }
+        
+        // Send audio to the speaker
+        do {
+            try avAudioSession.overrideOutputAudioPort(AVAudioSessionPortOverride.Speaker)
+        } catch let error1 as NSError {
+            error = error1
+            print("AppDelegate: could not overide output audio port")
+            if let e = error {
+                print(e.localizedDescription)
+            }
+        }
+        
+        // Activiate the AVAudioSession
+        do {
+            try avAudioSession.setActive(true)
+        } catch let error1 as NSError {
+            error = error1
+            print("AppDelegate: could not make session active")
+            if let e = error {
+                print(e.localizedDescription)
+            }
+        }
+        
+        let rwf = RWFramework.sharedInstance
+        rwf.addDelegate(self)
+        
+        return true
+    }
+    
+    func applicationWillResignActive(application: UIApplication) {
+        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+        // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+    }
+    
+    func applicationDidEnterBackground(application: UIApplication) {
+        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    }
+    
+    func applicationWillEnterForeground(application: UIApplication) {
+        // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+    }
+    
+    func applicationDidBecomeActive(application: UIApplication) {
+        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    }
+    
+    func applicationWillTerminate(application: UIApplication) {
+        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    }
+    
+    
+}
+
---
+++ Pod/Classes/RWFrameworkMediaUploader.swift
@@ -0,0 +1,104 @@
+//
+//  RWFrameworkMediaUploader.swift
+//  RWFramework
+//
+//  Created by Joe Zobkiw on 3/4/15.
+//  Copyright (c) 2015 Roundware. All rights reserved.
+//
+
+import Foundation
+import AVFoundation
+
+extension RWFramework {
+
+    /// Maximum number of times to try re-uploading beore we skip an item
+    var mediaRetryLimit: NSNumber {
+        get { return 3 }
+    }
+
+// MARK: Entry
+
+    /// Handles kicking off the upload of the next available item in the queue
+    func mediaUploader() {
+        if (uploaderActive == false) { return }
+        if (uploaderUploading == true) { return }
+        uploaderUploading = true
+
+        let media = getMediaToProcess()
+        if (media != nil) {
+            self.uploadMedia(media!)
+        } else {
+            self.uploaderUploading = false
+        }
+    }
+
+// MARK: Iterator
+
+    /// Return the next media item to process in the queue via completion routine, or nil
+    func getMediaToProcess() -> Media? {
+        for media in mediaArray {
+            if ((media.mediaStatus == MediaStatus.Ready) ||
+                (media.mediaStatus == MediaStatus.UploadFailed && media.retryCount.intValue < mediaRetryLimit.intValue)) {
+                return media
+            }
+            if (media.mediaStatus == MediaStatus.UploadFailed && media.retryCount.intValue >= mediaRetryLimit.intValue) {
+                self.deleteMediaFile(media)
+                self.removeMedia(media)
+            }
+        }
+        return nil
+    }
+
+// MARK: Uploader
+
+    /// Upload the passed media, after multiple attempts to upload a file will not be attempted further. See countUploadFailedMedia and purgeUploadFailedMedia to manage those failures
+    func uploadMedia(media: Media) {
+        media.mediaStatus = MediaStatus.Uploading
+
+        let bti = UIApplication.sharedApplication().beginBackgroundTaskWithName("RWFramework_uploadMedia", expirationHandler: { () -> Void in
+            // last ditch effort
+            self.logToServer("upload_failed", data: "RWFramework_uploadMedia background task expired.")
+            self.println("RWFramework couldn't finish uploading in time.")
+        })
+
+        // upload here
+        apiPatchEnvelopesId(media, success:{ () -> Void in
+            self.println("apiPatchEnvelopesId success")
+            media.mediaStatus = MediaStatus.UploadCompleted
+            self.deleteMediaFile(media)
+            self.removeMedia(media)
+            UIApplication.sharedApplication().endBackgroundTask(bti)
+            self.uploaderUploading = false
+        }, failure:{ (error: NSError) -> Void in
+            self.println("apiPatchEnvelopesId failure")
+            if (error.code == 400) {
+                self.deleteMediaFile(media)
+                self.removeMedia(media)
+            } else {
+                media.mediaStatus = MediaStatus.UploadFailed
+                media.retryCount = media.retryCount.integerValue + 1
+            }
+            UIApplication.sharedApplication().endBackgroundTask(bti)
+            self.uploaderUploading = false
+        })
+    }
+
+// MARK: Cleanup
+
+    func deleteMediaFile(media: Media) {
+        var error: NSError?
+        var b: Bool
+        do {
+            try NSFileManager.defaultManager().removeItemAtPath(media.string)
+            b = true
+        } catch let error1 as NSError {
+            error = error1
+            b = false
+        }
+        if let _ = error {
+            println("RWFramework - Couldn't delete media file after successful upload \(error)")
+        } else if (b == false) {
+            println("RWFramework - Couldn't delete media file after successful upload for an unknown reason")
+        }
+    }
+}
