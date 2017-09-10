6fdb5aaec05fd38af5ce574bf287031e3e1e3604
Joe Zobkiw <zobkiw@gmail.com>
2017-09-10 09:54:01 -0400

    listen buttons implemented and working
    play/stop/replay/skip

--- RWExample/RWExample/ListenViewController.swift
+++ RWExample/RWExample/ListenViewController.swift
@@ -14,15 +14,22 @@ class ListenViewController: UIViewController {
     
     // MARK: -
 
-    @IBOutlet var tagsButton: UIButton!
     @IBOutlet var playButton: UIButton!
+    @IBOutlet var replayButton: UIButton!
     @IBOutlet var skipButton: UIButton!
     @IBOutlet var stopButton: UIButton!
+
+    @IBOutlet var filterButton: UIButton!
+    @IBOutlet var moreButton: UIButton! // Flag Recording, Block Recording, Block User (apiPostAssetsIdVotes)
+    
 
     // MARK: -
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        playButton.isEnabled = true
+        filterButton.isEnabled = true
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -33,7 +40,48 @@ class ListenViewController: UIViewController {
         RWFramework.sharedInstance.removeDelegate(self)
     }
 
-}
+    // MARK: -
+
+    func updateUI() {
+        let rwf = RWFramework.sharedInstance
+
+        playButton.isEnabled = !rwf.isPlaying
+        stopButton.isEnabled = rwf.isPlaying
+        
+        replayButton.isEnabled = rwf.isPlaying
+        skipButton.isEnabled = rwf.isPlaying
+        moreButton.isEnabled = rwf.isPlaying
+    }
+    
+    @IBAction func play(_ sender: UIButton) {
+        let rwf = RWFramework.sharedInstance
+        rwf.play()
+        updateUI()
+    }
+    
+    @IBAction func stop(_ sender: UIButton) {
+        let rwf = RWFramework.sharedInstance
+        rwf.stop()
+        updateUI()
+    }
+    
+    @IBAction func filter(_ sender: UIButton) {
+        
+    }
+
+    @IBAction func replay(_ sender: UIButton) {
+        let rwf = RWFramework.sharedInstance
+        rwf.replay()
+    }
+
+    @IBAction func skip(_ sender: UIButton) {
+        let rwf = RWFramework.sharedInstance
+        rwf.skip()
+    }
+
+    @IBAction func more(_ sender: UIButton) {
+
+    }
 
 //    @IBAction func listenTags(_ sender: UIButton) {
 //        RWFramework.sharedInstance.editListenTags()
@@ -48,3 +96,5 @@ class ListenViewController: UIViewController {
 //    @IBAction func listenSkip(_ sender: UIButton) {
 //        RWFramework.sharedInstance.skip()
 //    }
+
+}
