58c9621bf4d22d679e9cc5f49e51f1dd8b394313
Halsey Burgund <halsey@halseyburgund.com>
2018-02-02 13:35:09 -0500

    POST streams/:id/ requires latitude and longitude params (#35)

--- RWFramework/RWFramework/RWFrameworkHTTP.swift
+++ RWFramework/RWFramework/RWFrameworkHTTP.swift
@@ -76,9 +76,9 @@ extension RWFramework: URLSessionDelegate, URLSessionTaskDelegate, URLSessionDat
         }
     }
 
-    func httpPostStreams(_ session_id: NSNumber, completion:@escaping (_ data: Data?, _ error: NSError?) -> Void) {
+    func httpPostStreams(_ session_id: NSNumber, latitude: String = "0", longitude: String = "0", completion:@escaping (_ data: Data?, _ error: NSError?) -> Void) {
         if let url = URL(string: RWFrameworkURLFactory.postStreamsURL()) {
-            let postData = ["session_id": session_id]
+            let postData = ["session_id": session_id, "latitude": latitude, "longitude": longitude] as [String:Any]
             postDataToURL(url, postData: postData, completion: completion)
         } else {
             let error = NSError(domain:self.reverse_domain, code:NSURLErrorBadURL, userInfo:[NSLocalizedDescriptionKey : "postStreamsURL unable to be created."])
