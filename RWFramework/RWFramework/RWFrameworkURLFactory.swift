d0452e2f25f9893dc4551266f0ba1b1bcb469c3a
Taylor Snead <taylorsnead@gmail.com>
2018-11-25 21:12:34 -0500

    Simplify api url factory

--- RWFramework/RWFramework/RWFrameworkURLFactory.swift
+++ RWFramework/RWFramework/RWFrameworkURLFactory.swift
@@ -11,116 +11,114 @@ import CoreLocation
 
 open class RWFrameworkURLFactory {
     class func api() -> String {
-        return "api/2/"
+        return RWFrameworkConfig.getConfigValueAsString("base_url") + "api/2"
     }
 
     class func postUsersURL() -> String {
-        return RWFrameworkConfig.getConfigValueAsString("base_url") + api() + "users/"
+        return "\(api())/users/"
     }
 
     class func postSessionsURL() -> String {
-        return RWFrameworkConfig.getConfigValueAsString("base_url") + api() + "sessions/"
+        return "\(api())/sessions/"
     }
 
     class func getProjectsIdURL(_ project_id: NSNumber, session_id: NSNumber) -> String {
-        return RWFrameworkConfig.getConfigValueAsString("base_url") + api() + "projects/" + project_id.stringValue + "/?session_id=" + session_id.stringValue
+        return "\(api())/projects/\(project_id.stringValue)/?session_id=\(session_id.stringValue)"
     }
 
     class func getProjectsIdTagsURL(_ project_id: NSNumber, session_id: NSNumber) -> String {
-        return RWFrameworkConfig.getConfigValueAsString("base_url") + api() + "projects/" + project_id.stringValue + "/tags/" + "?session_id=" + session_id.stringValue
+        return "\(api())/projects/\(project_id.stringValue)/tags/?session_id=\(session_id.stringValue)"
     }
-    
+
     class func getProjectsIdUIGroupsURL(_ project_id: NSNumber, session_id: NSNumber) -> String {
-        return RWFrameworkConfig.getConfigValueAsString("base_url") + api() + "projects/" + project_id.stringValue + "/uigroups/" + "?session_id=" + session_id.stringValue
+        return "\(api())/projects/\(project_id.stringValue)/uigroups/?session_id=\(session_id.stringValue)"
     }
 
     class func getTagCategoriesURL() -> String {
-        return RWFrameworkConfig.getConfigValueAsString("base_url") + api() + "tagcategories/"
+        return "\(api())/tagcategories/"
     }
-    
+
     class func getUIConfigURL(_ project_id: NSNumber, session_id: NSNumber) -> String {
-        return RWFrameworkConfig.getConfigValueAsString("base_url") + api() + "projects/" + project_id.stringValue + "/uiconfig/" + "?session_id=" + session_id.stringValue
+        return "\(api())/projects/\(project_id.stringValue)/uiconfig/?session_id=\(session_id.stringValue)"
     }
 
     class func postStreamsURL() -> String {
-        return RWFrameworkConfig.getConfigValueAsString("base_url") + api() + "streams/"
+        return "\(api())/streams/"
     }
 
     class func patchStreamsIdURL(_ stream_id: String) -> String {
-        return RWFrameworkConfig.getConfigValueAsString("base_url") + api() + "streams/" + stream_id + "/"
+        return "\(api())/streams/\(stream_id)/"
     }
 
     class func postStreamsIdHeartbeatURL(_ stream_id: String) -> String {
-        return RWFrameworkConfig.getConfigValueAsString("base_url") + api() + "streams/" + stream_id + "/heartbeat/"
+        return "\(api())/streams/\(stream_id)/heartbeat/"
     }
 
     class func postStreamsIdReplayURL(_ stream_id: String) -> String {
-        return RWFrameworkConfig.getConfigValueAsString("base_url") + api() + "streams/" + stream_id + "/replayasset/"
+        return "\(api())/streams/\(stream_id)/replayasset/"
     }
-    
+
     class func postStreamsIdSkipURL(_ stream_id: String) -> String {
-        return RWFrameworkConfig.getConfigValueAsString("base_url") + api() + "streams/" + stream_id + "/skipasset/"
+        return "\(api())/streams/\(stream_id)/skipasset/"
     }
-    
+
     class func postStreamsIdPauseURL(_ stream_id: String) -> String {
-        return RWFrameworkConfig.getConfigValueAsString("base_url") + api() + "streams/" + stream_id + "/pause/"
+        return "\(api())/streams/\(stream_id)/pause/"
     }
-    
+
     class func postStreamsIdResumeURL(_ stream_id: String) -> String {
-        return RWFrameworkConfig.getConfigValueAsString("base_url") + api() + "streams/" + stream_id + "/resume/"
+        return "\(api())/streams/\(stream_id)/resume/"
     }
-    
+
     class func getStreamsIdIsActiveURL(_ stream_id: String) -> String {
-        return RWFrameworkConfig.getConfigValueAsString("base_url") + api() + "streams/" + stream_id + "/isactive/"
+        return "\(api())/streams/\(stream_id)/isactive/"
     }
-    
+
     class func postEnvelopesURL() -> String {
-        return RWFrameworkConfig.getConfigValueAsString("base_url") + api() + "envelopes/"
+        return "\(api())/envelopes/"
     }
 
     class func patchEnvelopesIdURL(_ envelope_id: String) -> String {
-        return RWFrameworkConfig.getConfigValueAsString("base_url") + api() + "envelopes/" + envelope_id + "/"
+        return "\(api())/envelopes/\(envelope_id)/"
     }
 
     class func getTimedAssetsURL(_ dict: [String:String]) -> String {
-        return RWFrameworkConfig.getConfigValueAsString("base_url") + api() + "timedassets/" + dict.toUrlQuery()
+        return "\(api())/timedassets/\(dict.toUrlQuery())"
     }
 
     class func getAssetsURL(_ dict: [String:String]) -> String {
-        return RWFrameworkConfig.getConfigValueAsString("base_url") + api() + "assets/" + dict.toUrlQuery()
+        return "\(api())/assets/\(dict.toUrlQuery())"
     }
-    
+
     class func getAudioTracksURL(_ dict: [String:String]) -> String {
-        return RWFrameworkConfig.getConfigValueAsString("base_url") + api() + "audiotracks/" + dict.toUrlQuery()
+        return "\(api())/audiotracks/\(dict.toUrlQuery())"
     }
 
     class func getAssetsIdURL(_ asset_id: String) -> String {
-        return RWFrameworkConfig.getConfigValueAsString("base_url") + api() + "assets/" + asset_id + "/"
+        return "\(api())/assets/\(asset_id)/"
     }
 
     class func postAssetsIdVotesURL(_ asset_id: String) -> String {
-        return RWFrameworkConfig.getConfigValueAsString("base_url") + api() + "assets/" + asset_id + "/votes/"
+        return "\(api())/assets/\(asset_id)/votes/"
     }
 
     class func getAssetsIdVotesURL(_ asset_id: String) -> String {
-        return RWFrameworkConfig.getConfigValueAsString("base_url") + api() + "assets/" + asset_id + "/votes/"
+        return "\(api())/assets/\(asset_id)/votes/"
     }
 
     class func postEventsURL() -> String {
-        return RWFrameworkConfig.getConfigValueAsString("base_url") + api() + "events/"
-    }
-    
-    
-    class func getSpeakersURL(_ dict: [String:String]) -> String {
-        return RWFrameworkConfig.getConfigValueAsString("base_url") + api() + "speakers/" + dict.toUrlQuery()
+        return "\(api())/events/"
     }
 
+    class func getSpeakersURL(_ dict: [String:String]) -> String {
+        return "\(api())/speakers/\(dict.toUrlQuery())"
+    }
 }
 
 fileprivate extension Dictionary where Key == String, Value == String {
     func toUrlQuery() -> String {
         var result = ""
-        if (self.count > 0) {
+        if self.count > 0 {
             result += "?"
         }
         for (key, value) in self {
