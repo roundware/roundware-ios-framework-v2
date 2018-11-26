011f513f847e1dc6bbc8e1c90f003c140a2f5835
Taylor Snead <taylorsnead@gmail.com>
2018-11-25 21:11:34 -0500

    Implement simple panning based on angle to asset

--- RWFramework/RWFramework/Playlist/AudioTrack.swift
+++ RWFramework/RWFramework/Playlist/AudioTrack.swift
@@ -8,6 +8,8 @@
 
 import Foundation
 import StreamingKit
+import SwiftyJSON
+import CoreLocation
 
 /// An AudioTrack has a set of parameters determining how its audio is played.
 /// Assets are provided by the Playlist, so they must match any geometric parameters.
@@ -30,7 +32,7 @@ public class AudioTrack: NSObject, STKAudioPlayerDelegate {
     var currentAsset: Asset? = nil
     private var nextAsset: Asset? = nil
     private var fadeTimer: Timer? = nil
-    private var currentAssetDuration: Float? = nil
+    private var currentAssetDuration: Double? = nil
     private var fadeOutTimer: Timer? = nil
     
     init(
@@ -53,19 +55,16 @@ public class AudioTrack: NSObject, STKAudioPlayerDelegate {
     
     
     static func from(data: Data) throws -> [AudioTrack] {
-        let json = try JSONSerialization.jsonObject(with: data, options: JSONSerialization.ReadingOptions.mutableContainers)
-        
-        let items = json as! [AnyObject]
-        return items.map { obj in
-            let it = obj as! [String: AnyObject]
-            return AudioTrack(
-                id: it["id"] as! Int,
-                volume: (it["minvolume"] as! Float)...(it["maxvolume"] as! Float),
-                duration: (it["minduration"] as! Float)...(it["maxduration"] as! Float),
-                deadAir: (it["mindeadair"] as! Float)...(it["maxdeadair"] as! Float),
-                fadeInTime: (it["minfadeintime"] as! NSNumber).floatValue...(it["maxfadeintime"] as! NSNumber).floatValue,
-                fadeOutTime: (it["minfadeouttime"] as! NSNumber).floatValue...(it["maxfadeouttime"] as! NSNumber).floatValue,
-                repeatRecordings: it["repeatrecordings"] as! Bool
+        let items = try JSON(data: data).arrayValue
+        return items.map { it in
+            AudioTrack(
+                id: it["id"].intValue,
+                volume: (it["minvolume"].floatValue)...(it["maxvolume"].floatValue),
+                duration: (it["minduration"].floatValue)...(it["maxduration"].floatValue),
+                deadAir: (it["mindeadair"].floatValue)...(it["maxdeadair"].floatValue),
+                fadeInTime: (it["minfadeintime"].floatValue)...(it["maxfadeintime"].floatValue),
+                fadeOutTime: (it["minfadeouttime"].floatValue)...(it["maxfadeouttime"].floatValue),
+                repeatRecordings: it["repeatrecordings"].bool ?? false
             )
         }
     }
@@ -104,26 +103,48 @@ public class AudioTrack: NSObject, STKAudioPlayerDelegate {
             // Fallback on earlier versions
         }
     }
+
+    private func setDynamicVolume(at assetLoc: CLLocation, _ params: StreamParams) {
+        // scale volume based on the asset's distance from us
+        let dist = assetLoc.distance(from: params.location)
+        let distRange: ClosedRange<Double>
+        if let maxDist = params.maxDist, let minDist = params.minDist {
+            // if we have a custom distance range,
+            // interpolate asset volume within that.
+            distRange = minDist...maxDist
+        } else {
+            // otherwise, just use the default project distance to scale the asset volume
+            distRange = 0...playlist!.project.recording_radius
+        }
+        let distRatio = (dist - distRange.lowerBound) / distRange.difference
+        let clampedRatio = Float(distRatio.clamp(to: 0...1))
+
+        player.volume = volume.lowerBound + (clampedRatio * volume.difference)
+    }
+
+    private func setDynamicPan(at assetLoc: CLLocation, _ params: StreamParams) {
+        // TODO: Confirm this is degrees
+        guard let heading = params.heading else { return }
+        print("heading = \(heading)")
+        var angle = params.location.angle(to: assetLoc).radiansToDegrees - heading
+        // first, we need to map the angle into [-90, 90]
+        // 90<a<270: 95 => 85, 100 => 80, 190 => -10, 200 => -20
+        // 270<a<360: 270 => -90, 290 => -70, 300 => 0 - 60
+        if angle > 90 && angle <= 270 {
+            angle = 180 - angle
+        } else if angle > 270 && angle < 360 {
+            angle = angle - 360
+        }
+
+        // pan can be in the range [-1, 1]
+        // [-90, 90] => [-1, 1]
+        player.pan = Float(angle / 90.0)
+    }
     
     func updateParams(_ params: StreamParams) {
-        // TODO: Pan based on user and asset positions
-        if let asset = currentAsset, asset.location != nil {
-            // scale volume based on the asset's distance from us
-            // TODO: Confirm the ratio scaling. Should we be using the dynamic range or a project setting? Probably a setting.
-            let dist = Float(asset.location!.distance(from: params.location))
-            let distRange: ClosedRange<Float>
-            if let maxDist = params.maxDist, let minDist = params.minDist {
-                // if we have a custom distance range,
-                // interpolate asset volume within that.
-                distRange = Float(minDist)...Float(maxDist)
-            } else {
-                // otherwise, just use the default project distance to scale the asset volume
-                distRange = 0...Float(playlist!.project.recording_radius)
-            }
-            let distRatio = (dist - distRange.lowerBound) / distRange.difference
-            let clampedRatio = distRatio.clamp(to: 0...1)
-
-            player.volume = volume.lowerBound + (distRatio * volume.difference)
+        if let assetLoc = currentAsset?.location {
+            setDynamicVolume(at: assetLoc, params)
+            setDynamicPan(at: assetLoc, params)
         }
     }
     
@@ -153,11 +174,12 @@ public class AudioTrack: NSObject, STKAudioPlayerDelegate {
     private func fadeIn(cb: @escaping () -> Void = {}) {
         // pick a random duration
         // start at a random position between start and end - duration
-        let duration = self.duration.random()
+        let duration = Double(self.duration.random())
         let latestStart = currentAsset!.length - duration
-        let start = (0...latestStart).random()
-        
-        player.seek(toTime: Double(start))
+        let start = (0.0...latestStart).random()
+
+        //
+        player.seek(toTime: start)
         
         player.volume = 0.0
         let interval = 0.075 // seconds
@@ -224,16 +246,16 @@ public class AudioTrack: NSObject, STKAudioPlayerDelegate {
         setupFadeEndTimer()
     }
 
-    private func setupFadeEndTimer(endTime: Float = 0) {
+    private func setupFadeEndTimer(endTime: Double = 0) {
         // pick a fade-out length
         if currentAssetDuration == nil {
             var assetEndTime = endTime
             if assetEndTime == 0 {
                 assetEndTime = currentAsset!.length
             }
-            currentAssetDuration = assetEndTime - fadeOutTime.random()
+            currentAssetDuration = assetEndTime - Double(fadeOutTime.random())
         }
-        let fadeDur = Double(currentAssetDuration!) - player.progress
+        let fadeDur = currentAssetDuration! - player.progress
         
         fadeOutTimer?.invalidate()
         if #available(iOS 10.0, *) {
@@ -253,6 +275,11 @@ extension ClosedRange where Bound == Float {
         return lowerBound + Bound(drand48()) * (upperBound - lowerBound)
     }
 }
+extension ClosedRange where Bound == Double {
+    func random() -> Bound {
+        return lowerBound + drand48() * (upperBound - lowerBound)
+    }
+}
 
 extension ClosedRange where Bound: Numeric {
     var difference: Bound {
@@ -263,5 +290,23 @@ extension ClosedRange where Bound: Numeric {
 extension Comparable {
     func clamp(to: ClosedRange<Self>) -> Self {
         return max(to.lowerBound, min(to.upperBound, self))
+    }
+}
+
+extension CLLocation {
+    func angle(to destinationLocation: CLLocation) -> Double {
+        let lat1 = self.coordinate.latitude.degreesToRadians
+        let lon1 = self.coordinate.longitude.degreesToRadians
+
+        let lat2 = destinationLocation.coordinate.latitude.degreesToRadians
+        let lon2 = destinationLocation.coordinate.longitude.degreesToRadians
+
+        let dLon = lon2 - lon1
+
+        let y = sin(dLon) * cos(lat2)
+        let x = cos(lat1) * sin(lat2) - sin(lat1) * cos(lat2) * cos(dLon)
+        let radiansBearing = atan2(y, x)
+
+        return radiansBearing
     }
 }
