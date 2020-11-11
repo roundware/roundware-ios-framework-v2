d3020b183a604651b2548e73fef3e56dd6e665b0
Taylor Snead <taylorsnead@gmail.com>
2020-11-11 11:53:38 -0500

    :bug: Fix tag check logic

--- RWFramework/RWFramework/RWFrameworkTags.swift
+++ RWFramework/RWFramework/RWFrameworkTags.swift
@@ -10,32 +10,31 @@
  UIConfig struct in order to get from the server exactly what is needed for display in one result rather than having
  to piece together tags, tag categories and UI groups. It is recommended that you use the simplified mechanism if at
  all possible unless you specifically need more control over the raw data from the server.
-*/
+ */
 
 import Foundation
 
 extension RWFramework {
-
     // Tags
-    
+
     public struct Relationship: Codable {
         public var id: Int?
         public var tag_id: Int?
         public var parent_id: Int?
     }
-    
+
     public struct Location: Codable {
         public var type: String?
-        // TODO
+        // TODO:
     }
-    
+
     public struct Tag: Codable {
         public var id: Int
         public var value: String?
         public var description: String?
         public var data: String?
         public var filter: String?
-        //public var location: [Location]? // TODO
+        // public var location: [Location]? // TODO
         public var project_id: Int
         public var tag_category_id: Int
         public var description_loc: String?
@@ -44,7 +43,7 @@ extension RWFramework {
     }
 
     // UIGroups
-    
+
     public struct UIItem: Codable {
         public var id: Int
         public var index: Int
@@ -54,7 +53,7 @@ extension RWFramework {
         public var tag_id: Int?
         public var parent_id: Int?
     }
-    
+
     public struct UIGroup: Codable {
         public var id: Int
         public var name: String?
@@ -69,30 +68,29 @@ extension RWFramework {
     }
 
     // TagCategories
-    
+
     public struct TagCategory: Codable {
         public var id: Int
         public var name: String?
         public var data: String?
     }
-    
-// MARK: TagCategories
+
+    // MARK: TagCategories
 
     public func getTagCategories() -> [TagCategory]? {
         do {
             if let data = UserDefaults.standard.object(forKey: "tagcategories") {
                 let decoder = JSONDecoder()
                 let tagcategories = try decoder.decode([TagCategory].self, from: data as! Data)
-                return tagcategories;
+                return tagcategories
             }
-        }
-        catch {
+        } catch {
             print(error)
         }
         return nil
     }
-    
-    public func getFilteredTagCategories(_ ids: Array<Int>) -> [TagCategory]? {
+
+    public func getFilteredTagCategories(_ ids: [Int]) -> [TagCategory]? {
         var result = [TagCategory]()
         let tagcategories = getTagCategories()
         for id in ids {
@@ -101,16 +99,16 @@ extension RWFramework {
         }
         return result
     }
-    
-// MARK: UIGroups
-    
+
+    // MARK: UIGroups
+
     public func getUIGroups(_ ui_mode: String) -> [UIGroup]? {
         // Get groups
         do {
             if let data = UserDefaults.standard.object(forKey: "ui_groups") {
                 let decoder = JSONDecoder()
                 let uigrouplist = try decoder.decode([UIGroup].self, from: data as! Data)
-                
+
                 // Create an empty array of groups that will be filled with all listen tags
                 var groups = [UIGroup]()
 
@@ -118,7 +116,7 @@ extension RWFramework {
                 for group in uigrouplist where group.ui_mode == ui_mode {
                     groups.append(group)
                 }
-                
+
                 guard groups.count > 0 else { return nil }
                 groups.sort {
                     return $0.index < $1.index
@@ -126,8 +124,7 @@ extension RWFramework {
 
                 return groups
             }
-        }
-        catch {
+        } catch {
             print(error)
         }
         return nil
@@ -142,9 +139,9 @@ extension RWFramework {
         }
         return ids
     }
-    
-// MARK: Tags
-    
+
+    // MARK: Tags
+
     public func getTags(_ ui_mode: String) -> [Tag]? {
         // Get tags and groups
         do {
@@ -158,18 +155,15 @@ extension RWFramework {
 
                 // Iterate groups for ui_mode of the passed in parameter
                 for group in uigrouplist! where group.ui_mode == ui_mode {
-
                     // Verify that there are ui_items defined for any found group
                     guard let ui_items = group.ui_items else { continue }
 
                     // Iterate the ui_items
                     for ui_item in ui_items {
-
                         // Verify that there is a tag_id set for any found item
                         if let tag_id = ui_item.tag_id {
-
                             // Find the tag with that tag_id
-                            if let tag = taglist.filter({$0.id == tag_id}).first {
+                            if let tag = taglist.filter({ $0.id == tag_id }).first {
                                 tags.append(tag)
                             }
                         }
@@ -178,34 +172,29 @@ extension RWFramework {
                 guard tags.count > 0 else { return nil }
                 return tags
             }
-        }
-        catch {
+        } catch {
             print(error)
         }
         return nil
     }
-    
+
     public func getDefaultTags(_ ui_mode: String) -> [Tag]? {
         // Get tags and groups
         if let uigrouplist = getUIGroups(ui_mode), let taglist = getTags(ui_mode) {
-            
             // Create an empty array of tags that will be filled with all ui_mode tags
             var tags = [Tag]()
-            
+
             // Iterate groups for ui_mode of the passed in parameter
             for group in uigrouplist where group.ui_mode == ui_mode {
-                
                 // Verify that there are ui_items defined for any found group
                 guard let ui_items = group.ui_items else { continue }
-                
+
                 // Iterate the ui_items
                 for ui_item in ui_items {
-                    
                     // Verify that there is a tag_id set for any found item and that it is set to be a default
                     if let tag_id = ui_item.tag_id, ui_item.default == true {
-                        
                         // Find the tag with that tag_id
-                        if let tag = taglist.filter({$0.id == tag_id}).first {
+                        if let tag = taglist.filter({ $0.id == tag_id }).first {
                             tags.append(tag)
                         }
                     }
@@ -217,8 +206,8 @@ extension RWFramework {
         return nil
     }
 
-// MARK: Simplified
-    
+    // MARK: Simplified
+
     public struct UIConfigItem: Codable {
         public var id: Int
         public var tag_id: Int
@@ -226,40 +215,39 @@ extension RWFramework {
         public var default_state: Bool
         public var tag_display_text: String?
     }
-    
+
     public struct UIConfigGroup: Codable {
         public var select: String?
         public var group_short_name: String?
         public var header_display_text: String?
         public var display_items: [UIConfigItem]
     }
-    
-    public struct UIConfig : Codable {
+
+    public struct UIConfig: Codable {
         public var speak: [UIConfigGroup]
         public var listen: [UIConfigGroup]
     }
-    
+
     public func getUIConfig() -> UIConfig? {
         do {
             if let data = UserDefaults.standard.object(forKey: "uiconfig") {
                 let decoder = JSONDecoder()
                 let uiconfig = try decoder.decode(UIConfig.self, from: data as! Data)
-                return uiconfig;
+                return uiconfig
             }
-        }
-        catch {
+        } catch {
             print(error)
         }
         return nil
     }
-    
-// MARK: --
 
-    public func submitListenIDsSetAsTags(streamPatchOptions: [String: Any] = [:]) {
+    // MARK: - -
+
+    public func submitListenIDsSetAsTags(streamPatchOptions _: [String: Any] = [:]) {
         // Tell the playlist that it can check for newly available assets
-        self.updateStreamParams()
+        updateStreamParams()
     }
-    
+
     public func getListenTagIDFromID(_ id: Int) -> Int {
         if let uiconfig = getUIConfig() {
             for listen in uiconfig.listen {
@@ -272,7 +260,7 @@ extension RWFramework {
         }
         return 0
     }
-    
+
     public func getSubmittableListenIDsSetAsTags() -> String {
         var tag_ids: String = ""
         if let selectedIDs = getListenIDsSet() {
@@ -286,7 +274,7 @@ extension RWFramework {
         }
         return tag_ids
     }
-    
+
     public func getSubmittableListenTagIDsSet() -> Set<Int>? {
         var tag_ids: Set<Int> = []
         if let selectedIDs = getListenIDsSet() {
@@ -297,15 +285,15 @@ extension RWFramework {
         }
         return tag_ids
     }
-    
+
     public func tagIsEnabled(_ requestedTag: Int) -> Bool {
-        if let array = UserDefaults.standard.object(forKey: "listenIDsSet") as? Array<Int> {
+        if let array = UserDefaults.standard.object(forKey: "listenIDsSet") as? [Int] {
             return array.contains(requestedTag)
         } else {
             if let uiconfig = getUIConfig() {
                 for listen in uiconfig.listen {
                     for item in listen.display_items {
-                        if item.default_state == true {
+                        if item.default_state == true, item.tag_id == requestedTag {
                             return true
                         }
                     }
@@ -316,7 +304,7 @@ extension RWFramework {
     }
 
     public func getListenIDsSet() -> Set<Int>? {
-        if let array = UserDefaults.standard.object(forKey: "listenIDsSet") as? Array<Int> {
+        if let array = UserDefaults.standard.object(forKey: "listenIDsSet") as? [Int] {
             return Set(array)
         } else {
             if let uiconfig = getUIConfig() {
@@ -333,15 +321,15 @@ extension RWFramework {
         }
         return nil
     }
-    
+
     public func setListenIDsSet(_ ids: Set<Int>, streamPatchOptions: [String: Any] = [:]) {
         UserDefaults.standard.set(Array(ids), forKey: "listenIDsSet")
         UserDefaults.standard.synchronize()
         submitListenIDsSetAsTags(streamPatchOptions: streamPatchOptions)
     }
-    
+
     public func getFilterListenIDsSet() -> Set<Int>? {
-        if let array = UserDefaults.standard.object(forKey: "filterListenIDsSet") as? Array<Int> {
+        if let array = UserDefaults.standard.object(forKey: "filterListenIDsSet") as? [Int] {
             return Set(array)
         } else {
             if let uiconfig = getUIConfig() {
@@ -358,14 +346,14 @@ extension RWFramework {
         }
         return nil
     }
-    
+
     public func setFilterListenIDsSet(_ ids: Set<Int>) {
         UserDefaults.standard.set(Array(ids), forKey: "filterListenIDsSet")
         UserDefaults.standard.synchronize()
     }
 
-// MARK: --
-    
+    // MARK: - -
+
     public func getSpeakTagIDFromID(_ id: Int) -> Int {
         if let uiconfig = getUIConfig() {
             for speak in uiconfig.speak {
@@ -394,7 +382,7 @@ extension RWFramework {
     }
 
     public func getSpeakIDsSet() -> Set<Int>? {
-        if let array = UserDefaults.standard.object(forKey: "speakIDsSet") as? Array<Int> {
+        if let array = UserDefaults.standard.object(forKey: "speakIDsSet") as? [Int] {
             return Set(array)
         } else {
             if let uiconfig = getUIConfig() {
@@ -411,12 +399,12 @@ extension RWFramework {
         }
         return nil
     }
-    
+
     public func setSpeakIDsSet(_ ids: Set<Int>) {
         UserDefaults.standard.set(Array(ids), forKey: "speakIDsSet")
         UserDefaults.standard.synchronize()
     }
-    
+
     public func getSpeakIDsDefaultSet() -> Set<Int>? {
         if let uiconfig = getUIConfig() {
             var set = Set<Int>()
@@ -431,9 +419,9 @@ extension RWFramework {
         }
         return nil
     }
-    
-// MARK: --
-    
+
+    // MARK: - -
+
     public func getValidDisplayItems(_ group: [UIConfigGroup], index: Int, tags: Set<Int>) -> [UIConfigItem] {
         var display_items = [UIConfigItem]()
         for item in group[index].display_items {
@@ -455,7 +443,7 @@ extension RWFramework {
     public func getIDOfDisplayItemParent(_ display_item: UIConfigItem, group: [UIConfigGroup]) -> Int? {
         for g in group {
             for item in g.display_items {
-                if (item.id == display_item.parent_id) {
+                if item.id == display_item.parent_id {
                     return item.id
                 }
             }
