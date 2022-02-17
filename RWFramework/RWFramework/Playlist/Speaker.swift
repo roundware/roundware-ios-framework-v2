690fcbd694df2e66d7dfe0f5c4f07466c38bcd9e
Taylor Snead <taylor@snead.xyz>
2022-02-16 20:50:36 -0800

    Convert attenuation border lines into polygons

--- RWFramework/RWFramework/Playlist/Speaker.swift
+++ RWFramework/RWFramework/Playlist/Speaker.swift
@@ -11,6 +11,7 @@ import Repeat
  */
 public class Speaker: Codable {
     private static let fadeDuration: Float = 3.0
+    private static let fadeDeltaTime: Float = 0.05
     
     let id: Int
     let url: String
@@ -24,10 +25,21 @@ public class Speaker: Codable {
     let shape: Geometry
     let attenuationBorder: Geometry
     
-    private var player: AVPlayer? = nil
+    private lazy var player: AVPlayer = {
+        return AVPlayer(url: URL(string: self.url)!)
+    }()
+    private lazy var attenuationShape: Geometry = {
+        if case let .lineString(x) = attenuationBorder {
+            return .polygon(Polygon(exterior: try! Polygon.LinearRing(points: x.points)))
+        } else {
+            // We don't know how to handle other shapes.
+            return try! attenuationBorder.convexHull()
+        }
+    }()
     private var looper: Any? = nil
     private var fadeTimer: Repeater? = nil
     private var ignoringUpdates: Bool = false
+    private var volumeTarget: Float = 0.0
 
 
     enum CodingKeys: String, CodingKey {
@@ -52,7 +64,7 @@ extension Speaker {
     }
     
     private func attenuationShapeContains(_ point: CLLocation) -> Bool {
-        return try! point.toWaypoint().isWithin(attenuationBorder.convexHull())
+        return try! point.toWaypoint().isWithin(attenuationShape)
     }
     
     public func distance(to loc: CLLocation) -> Double {
@@ -72,11 +84,14 @@ extension Speaker {
     
     func volume(at point: CLLocation) -> Float {
         if attenuationShapeContains(point) {
+            // The "attenuation shape" is the inner area where volume should be maxed out.
             return volume.upperBound
         } else if contains(point) {
+            // The "shape" is the outer area where the speaker should play at variable loudness.
             let range = volume.difference
             return volume.lowerBound + range * Float(attenuationRatio(at: point))
         } else {
+            // Outside the "shape" is where the speaker should be at minimum volume.
             return volume.lowerBound
         }
     }
@@ -91,59 +106,58 @@ extension Speaker {
     }
 
     func syncTime(_ timeSinceStart: TimeInterval) {
-        if Speaker.shouldSync {
+        if Speaker.shouldSync && volumeTarget > 0.0 {
             let t = abs(timeSinceStart)
-            let timescale = self.player?.currentItem?.asset.duration.timescale ?? 1000
-            self.player?.seek(to: CMTime(seconds: t, preferredTimescale: timescale))
+            let timescale = self.player.currentItem?.asset.duration.timescale ?? 1000
+            self.player.seek(to: CMTime(seconds: t, preferredTimescale: timescale))
         }
     }
     
     @discardableResult
     func updateVolume(_ vol: Float, timeSinceStart: TimeInterval) -> Float {
         if self.ignoringUpdates {
-            return self.player?.volume ?? vol
+            return self.player.volume
         }
         
         if vol > 0.05 {
-            // definitely want to create the player if it needs volume
-            if self.player == nil {
-                player = AVPlayer(url: URL(string: url)!)
-
-                // Only loop non-synced speakers
-                if Speaker.shouldSync {
-                    player?.actionAtItemEnd = .none
-                } else {
-                    looper = looper ?? NotificationCenter.default.addObserver(forName: .AVPlayerItemDidPlayToEndTime, object: player!.currentItem, queue: .main) { [weak self] _ in
-                        self?.player?.seek(to: CMTime.zero)
-                        self?.player?.play()
-                    }
+            // Only loop non-synced speakers
+            if Speaker.shouldSync {
+                player.actionAtItemEnd = .none
+            } else {
+                looper = looper ?? NotificationCenter.default.addObserver(forName: .AVPlayerItemDidPlayToEndTime, object: player.currentItem, queue: .main) { [weak self] _ in
+                    self?.player.seek(to: CMTime.zero)
+                    self?.player.play()
                 }
-            }
-            // make sure this speaker is playing if it needs to be audible
-            if player!.rate.isZero && RWFramework.sharedInstance.isPlaying {
-                self.resume(timeSinceStart)
             }
         }
         
-        fadeTimer?.removeAllObservers(thenStop: true)
-        if let player = self.player, abs(vol - player.volume) > 0.02 {
-            let totalDiff = vol - player.volume
-            let delta: Float = 0.05
-            fadeTimer = .every(.seconds(Double(delta))) { timer in
-                let currDiff = vol - player.volume
-                if currDiff.sign != totalDiff.sign || abs(currDiff) < 0.05 {
-                    // we went just enough or too far
-                    player.volume = vol
-                    
-                    if vol < 0.05 {
-                        // we can't hear it anymore, so pause it.
-                        player.pause()
-                    } else if player.rate.isZero {
-                        self.resume(timeSinceStart)
+        // make sure this speaker is playing if it needs to be audible
+        if player.rate.isZero && RWFramework.sharedInstance.isPlaying {
+            self.resume(timeSinceStart)
+        }
+        
+        if abs(vol - self.volumeTarget) > 0.02 {
+            self.volumeTarget = vol
+            print("speaker \(self.id) target volume = \(vol)")
+            if fadeTimer?.state != .running {
+                let totalDiff = self.volumeTarget - player.volume
+                fadeTimer?.removeAllObservers(thenStop: true)
+                fadeTimer = .every(.seconds(Double(Speaker.fadeDeltaTime))) { timer in
+                    let currDiff = self.volumeTarget - self.player.volume
+                    if currDiff.sign != totalDiff.sign || abs(currDiff) < 0.05 {
+                        // we went just enough or too far
+                        self.player.volume = self.volumeTarget
+                        
+                        if self.player.volume < 0.05 {
+                            // we can't hear it anymore, so pause it.
+                            self.player.pause()
+                        } else if self.player.rate.isZero {
+                            self.resume(timeSinceStart)
+                        }
+                        timer.removeAllObservers(thenStop: true)
+                    } else {
+                        self.player.volume += totalDiff * Speaker.fadeDeltaTime / Speaker.fadeDuration
                     }
-                    timer.removeAllObservers(thenStop: true)
-                } else {
-                    player.volume += totalDiff * delta / Speaker.fadeDuration
                 }
             }
         }
@@ -152,32 +166,36 @@ extension Speaker {
     }
     
     func resume(_ timeSinceStart: TimeInterval) {
-        print("speaker resuming at \(timeSinceStart)")
         // Resuming a speaker implies coming back from a fully stopped state.
         // This allows us to easily reset the session.
         self.ignoringUpdates = false
-        syncTime(timeSinceStart)
-        player?.play()
+        if volumeTarget > 0.0 && player.rate.isZero {
+            print("speaker resuming at \(timeSinceStart)")
+            syncTime(timeSinceStart)
+            player.play()
+        }
     }
     
     func pause() {
-        player?.pause()
+        if volumeTarget > 0.0 {
+            player.pause()
+        }
     }
     
     public func fadeOutAndStop(for fadeDuration: Float) {
         self.ignoringUpdates = true
-        if let player = self.player {
+        if volumeTarget > 0.0 {
             let totalDiff = -player.volume
-            let delta: Float = 0.05
-            fadeTimer = .every(.seconds(Double(delta))) { timer in
-                if player.volume < 0.01 {
+            fadeTimer?.removeAllObservers(thenStop: true)
+            fadeTimer = .every(.seconds(Double(Speaker.fadeDeltaTime))) { timer in
+                if self.player.volume < 0.01 {
                     // we went just enough or too far
-                    player.volume = 0.0
+                    self.player.volume = 0.0
                     // we can't hear it anymore, so pause it.
-                    player.pause()
+                    self.player.pause()
                     timer.removeAllObservers(thenStop: true)
                 } else {
-                    player.volume += totalDiff * delta / fadeDuration
+                    self.player.volume += totalDiff * Speaker.fadeDeltaTime / fadeDuration
                 }
             }
         }
